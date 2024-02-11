4648968148744c95a8e1f6349960c79652c21738
JulianRB <ignis.lykaios@gmail.com>
2024-02-11 11:38:27 -0600

    Prueba para usar Pipedream

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -17,29 +17,26 @@ const getUsers = catchAsync(async function (req, res, next) {
 
 const createUser = catchAsync(async function (req, res, next) {
   const { username, email } = req.body;
-  // const user = await User.findOne({ email });
-  // if (user) {
-  //   user.engaged = true;
-  //   user.new = true;
-  //   user.active = true;
-  //   user.save();
-  //   res.json({ user });
-  // }
-  // if (!user) {
-  //   const newUser = await User.create({
-  //     username,
-  //     email,
-  //   });
-  //   res.json({ user: newUser });
-  // }
-  const contact = await activeCampaignApi.createContact(req.body);
-  if (!contact)
-    next(
-      new AppError(
-        'Algo pasó en el servidor, vuelve a intentarlo por favor',
-        500
-      )
-    );
+  const user = await User.findOne({ email });
+  if (user) {
+    user.save();
+    res.json({ user });
+  }
+  if (!user) {
+    const newUser = await User.create({
+      username,
+      email,
+    });
+    res.json({ user: newUser });
+  }
+  // const contact = await activeCampaignApi.createContact(req.body);
+  // if (!contact)
+  //   next(
+  //     new AppError(
+  //       'Algo pasó en el servidor, vuelve a intentarlo por favor',
+  //       500
+  //     )
+  //   );
 });
 
 module.exports = {
