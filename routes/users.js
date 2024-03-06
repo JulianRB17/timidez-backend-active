a01b3c30ada57a96880d11ff07a897c936c7ad14
JulianRB <ignis.lykaios@gmail.com>
2024-03-05 19:03:28 -0600

    comentado delete users

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ const { celebrateCreateUserMiddleware } = require('../middlewares/celebrate');
 
 const {
   createUser,
-  getUsers,
-  deleteUsers,
+  // getUsers,
+  // deleteUsers,
 } = require('../controllers/usersController');
 
-usersRoute.get('/', getUsers);
+// usersRoute.get('/', getUsers);
 usersRoute.post('/', celebrateCreateUserMiddleware(), createUser);
-usersRoute.delete('/', deleteUsers);
+// usersRoute.delete('/:id', deleteUsers);
 
 module.exports = { usersRoute };
